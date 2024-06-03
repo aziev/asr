d0273de2bb87f0005934380d4a3918bca127d44b
aziev <mikail.aziev@gmail.com>
2024-06-03 11:43:22 +0300

    deploy

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = 'cache-first-v6'
+const cacheName = 'network-first-v1'
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(caches.keys().then((keyList) => {
@@ -11,33 +11,21 @@ self.addEventListener('activate', (event) => {
 })
 
 self.addEventListener('fetch', (event) => {
-  let typesToCache = [
-    'document',
-    'font',
-    'image',
-    'manifest',
-    'script',
-    'style',
-  ]
+  // Check if this is a navigation request
+  if (event.request.mode === 'navigate') {
+    // Open the cache
+    event.respondWith(caches.open(cacheName).then((cache) => {
+      // Go to the network first
+      return fetch(event.request.url).then((fetchedResponse) => {
+        cache.put(event.request, fetchedResponse.clone());
 
-  if (!typesToCache.includes(event.request.destination)) return
-
-  event.respondWith(caches.open(cacheName).then((cache) => {
-    // Go to the cache first
-    return cache.match(event.request.url).then((cachedResponse) => {
-      // Return a cached response if we have one
-      if (cachedResponse) {
-        return cachedResponse
-      }
-
-      // Otherwise, hit the network
-      return fetch(event.request).then((fetchedResponse) => {
-        // Add the network response to the cache for later visits
-        cache.put(event.request, fetchedResponse.clone())
-
-        // Return the network response
-        return fetchedResponse
-      })
-    })
-  }))
-})
+        return fetchedResponse;
+      }).catch(() => {
+        // If the network is unavailable, get
+        return cache.match(event.request.url);
+      });
+    }));
+  } else {
+    return;
+  }
+});
